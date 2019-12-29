de27cb1e98b3914ab1ed2e399500d34f87d91efd
alexgitrepo <alex.dev4web.com>
2019-12-29 11:12:07 +0500

    some css

--- src/components/Buttons/Button/Button.jsx
+++ src/components/Buttons/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = (props) => {
     return (
-        <button disabled={props.isDisabled} onClick={()=>{props.callbackFunction()}}>{props.title}</button>
+        <button className='button' disabled={props.isDisabled} onClick={()=>{props.callbackFunction()}}>{props.title}</button>
     );
 }
 
