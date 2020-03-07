7b5e59fae5b09a8e4b8c720552a05476cdc8c31a
alexgitrepo <alex.dev4web.com>
2020-03-07 12:39:51 +0500

    Updates

---
+++ precache-manifest.d8d75e88ccc1d15e597687dfe0361d85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805682152fc616a377279d0f7e1ee1eb",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "0be3e042b9582beea9e7",
+    "url": "/counter/static/css/main.67f37493.chunk.css"
+  },
+  {
+    "revision": "a817550fd3b0abfddf1b",
+    "url": "/counter/static/js/2.5ca611f4.chunk.js"
+  },
+  {
+    "revision": "289b3f4df53c5ff9b0c2e00d43955b44",
+    "url": "/counter/static/js/2.5ca611f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0be3e042b9582beea9e7",
+    "url": "/counter/static/js/main.71acf93a.chunk.js"
+  },
+  {
+    "revision": "0df1d2ae5fe9633b0af2",
+    "url": "/counter/static/js/runtime-main.4e53bc25.js"
+  }
+]);
